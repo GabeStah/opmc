0154f649195bb4bf541693983537857177ba3e39
Gabe Wyatt <gwyattkelsey@gmail.com>
2019-11-29 17:19:42 -0800

    Added Netlify auth

---
+++ api/authenticate.js
@@ -0,0 +1,15 @@
+exports.handler = function(event, context, callback) {
+  // your server-side functionality
+  console.log(event)
+  if (event.queryStringParameters.password && event.queryStringParameters.password === process.env.OPMC_MEMBER_PASSWORD) {
+    callback(null, {
+      statusCode: 200,
+      body: true
+    });
+  } else {
+    callback(null, {
+      statusCode: 200,
+      body: false
+    });
+  }
+}
