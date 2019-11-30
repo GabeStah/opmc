b66101478c25538b235b308b256c40bc2bc1cf03
Gabe Wyatt <gwyattkelsey@gmail.com>
2019-11-29 20:48:29 -0800

    Fixed post image path adding 'static/' prefix

--- src/components/post.js
+++ src/components/post.js
@@ -1,40 +1,43 @@
-import React, { Component } from "react"
+import React, { Component, useState } from "react"
 import moment from "moment"
 import Img from "gatsby-image"
+import { graphql, useStaticQuery } from "gatsby"
 
-export default class Post extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      post: props.post || [],
-    }
-  }
-
-  render() {
-    return (
-      <section className="bg-white py-8">
-        <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-          <div className="w-full sm:w-1/2 p-6">
-            {this.state.post.image && (
-                <img
-                  src={this.state.post.image}
-                  alt={this.state.post.image.alt_text || ""}
-                />
-              )}
-          </div>
-          <h1 className="w-full font-bold text-3xl text-gray-800 px-6">
-            {this.state.post.title}
-          </h1>
-          <h5 className={"w-full text-sm text-gray-500 px-6"}>
-            {moment(this.state.post.date).fromNow()}
-          </h5>
-          <div
-            className="text-gray-800 px-6 mb-5"
-            dangerouslySetInnerHTML={{ __html: this.state.post.content }}
-          />
+export default (props) => {
+  const [post, setPost] = useState(props.post)
+  // const data = useStaticQuery(graphql`
+  //     query {
+  //         image: file(relativePath: { eq: ${post.image} }) {
+  //             childImageSharp {
+  //                 fluid(maxWidth: 1000) {
+  //                     ...GatsbyImageSharpFluid
+  //                 }
+  //             }
+  //         }
+  //     }
+  // `)
+  return (
+    <section className="bg-white py-8">
+      <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+        <div className="w-full sm:w-1/2 p-6">
+          {post.image && (
+            <img
+              src={post.image.replace("static/", "")}
+              alt={post.image.alt_text || ""}
+            />
+          )}
         </div>
-      </section>
-    )
-  }
+        <h1 className="w-full font-bold text-3xl text-gray-800 px-6">
+          {post.title}
+        </h1>
+        <h5 className={"w-full text-sm text-gray-500 px-6"}>
+          {moment(post.date).fromNow()}
+        </h5>
+        <div
+          className="text-gray-800 px-6 mb-5"
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />
+      </div>
+    </section>
+  )
 }
-
