8da8bdcac651312da8f142712cedf34a012cf1e0
Gabe Wyatt <gwyattkelsey@gmail.com>
2019-11-29 17:54:59 -0800

    Added gatsby netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -181,6 +181,7 @@ module.exports = {
         },
       },
     },
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-postcss`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
